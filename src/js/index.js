7b66b23e90fd47223a5b557460f3c05a2d884fbe
Renato Bruno Lopes Coimbra Setúbal <133555561+RenatoCoimbra@users.noreply.github.com>
2023-05-24 17:43:59 -0300

    Update index.js

--- src/js/index.js
+++ src/js/index.js
@@ -4,10 +4,10 @@ const body = document.querySelector("body");
 
 const imagemBotaoTrocaDeTema = document.querySelector(".sol-lua");
 
+const modoEscuroEstaAtivo = body.classList.contains("modo-escuro");
+
 botaoAlterarTema.addEventListener("click", () => {
     
-    const modoEscuroEstaAtivo = body.classList.contains("modo-escuro");
-
     body.classList.toggle("modo-escuro");
 
     if (modoEscuroEstaAtivo) {
